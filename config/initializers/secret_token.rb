8a39d40abcdab21f503fae302ba8ff2b7f8629c8
abhishek <bigbeliever@gmail.com>
2013-11-22 23:07:41 +0530

    Spree added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpreeRails4::Application.config.secret_key_base = '463e7b96400a6ed1edd3acea62d228575c5f28792eef1975669545cae7f1f79c3c90fe1ca6ba956cbb415eef4cae27eaac3ec04bdc0540eb3552dde23b14f521'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SpreeRails4::Application.config.session_store :cookie_store, key: '_spree_rails4_session'
